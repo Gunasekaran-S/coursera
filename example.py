9ef2ee7ed7e43316c76277ee12bd7e94fb90f95c
Gunasekaran-S <96422438+Gunasekaran-S@users.noreply.github.com>
2021-12-25 11:18:48 +0530

    Added new function

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 def git_opeation():
     return "I am adding example.py file to the remote repository."
 
+def git_call():
+    return "new function added in the web".
 
 print(git_opeation())
+print(git_call())
